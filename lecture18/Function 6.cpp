88c5fcb86a2d6ecaa6ad7f79cecd42f32fa63903
Siddanth Gaikwad <119507554+siddanthgaikwad@users.noreply.github.com>
2024-07-25 19:17:22 +0530

    Function 6.cpp

--- lecture18/Function 6.cpp
+++ lecture18/Function 6.cpp
@@ -1,17 +1,41 @@
 #include<iostream>
 using namespace std;
 
-void swap(int &a, int &b){//this is swap function 
-    int temp;
-    temp = a;
-    a = b;
-    b = temp;
-    cout<<a<<" "<<b<<endl<<"values of a and b after swapping inside function : "<<a<<" "<<b<<endl ;
+//void swap(int a, int b){
+////	a=a+b;
+////	b=a-b;
+////	a=a-b;
+//    int c;
+//	c=a;
+//	a=b;
+//	b=c;	
+//	cout<<"After swap: "<<a<<" "<<b;
+//}
+void swap(int &a, int &b){   //we can change actual parameters, this method is called passing by reference
+	int c;
+	c=a;
+	a=b;
+	b=c;	
+	cout<<"After swap: "<<a<<" "<<b<<endl;
 }
 
-int main(){ 
-    int a =12, b=45;
-    swap(a,b);
-    cout<<"values of a and b after swapping outside function : "<<a<<" "<<b;
-    return 0;
+void func(int &a, int b){
+	a=9999;
+	b=9999;
+	cout<<"values of a & b in func: "<<a<<" "<<b<<endl;
+}
+
+int main(){
+	int a,b;
+	a=4;
+	b=6;
+	
+	cout<<"Before swap: "<<a<<" "<<b<<endl;
+	swap(a,b);
+	cout<<"after swap in main func: "<<a<<" "<<b<<endl;
+	
+	
+	func(a,b);
+	cout<<"values of a & b in main: "<<a<<" "<<b<<endl; //a is changed to 9999, b doesn't change
+	return 0;
 }
